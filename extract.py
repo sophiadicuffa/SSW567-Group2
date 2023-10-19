2b389fb935fb73fc2aa5176b31587af5c4347d21
Christopher Roddy <76755988+cjrkoa@users.noreply.github.com>
2023-10-18 22:30:39 -0400

    progress?
    I saw the two keys in the response were payload and title, this definitely isn't all of the data though

--- extract.py
+++ extract.py
@@ -1,14 +1,17 @@
 import requests
+import json
 import csv
 
 url = "https://github.com/NAIST-SE/DevGPT/tree/main/snapshot_20230727"
 response = requests.get(url)
 data = response.json()
 
-extracted_data = data["key"]["nested_key"]  # Adjust this based on your JSON structure
+extracted_data = {}
+extracted_data["payload"] = data["payload"]
+extracted_data["title"] = data["title"]
 
 with open('output.csv', 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(['Column1', 'Column2', ...])  # Add column headers
-    for item in extracted_data:
-        csv_writer.writerow([item['key1'], item['key2'], ...])  # Add data based on JSON structure
+    csv_writer.writerow(['data'])  # Add column headers
+    for key in extracted_data:
+        csv_writer.writerow(extracted_data[key])
