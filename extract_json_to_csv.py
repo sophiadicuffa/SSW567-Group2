ac5e2dacccebd757a851afa4d4475aad61598756
sophiadicuffa <sophiadicuffa@gmail.com>
2023-10-20 12:28:38 -0400

    817 extraction

--- extract_json_to_csv.py
+++ extract_json_to_csv.py
@@ -2,7 +2,7 @@ import requests
 import csv
 import json
 
-json_url = "https://raw.githubusercontent.com/NAIST-SE/DevGPT/main/snapshot_20230727/20230727_200003_commit_sharings.json"
+json_url = "https://raw.githubusercontent.com/NAIST-SE/DevGPT/main/snapshot_20230817/20230817_131244_commit_sharings.json"
 
 def extract_json_to_csv(json_data):
     '''Function to extract ChatgptSharing URLs and Number of Prompts, and write to CSV'''
@@ -17,7 +17,9 @@ def extract_json_to_csv(json_data):
             number_of_prompts.append(item.get("NumberOfPrompts"))
     
     # Writing URLs and Number of Prompts to CSV with numbering
-    with open("chatgpt_sharing_urls.csv", "w", newline="", encoding="utf-8") as csvfile:
+    
+    # Change the first three numbers to your own .json folder numbers
+    with open("817_chatgpt_sharing_urls.csv", "w", newline="", encoding="utf-8") as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(["ID", "URL", "Number of Prompts"])  
         for idx, (url, prompts) in enumerate(zip(chatgpt_sharing_urls, number_of_prompts), start=1):
