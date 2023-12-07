9627388bbd137f369cd149aa59ef0f61bbf04838
Christopher Roddy <76755988+cjrkoa@users.noreply.github.com>
2023-12-07 14:21:18 -0500

    filter code working

--- Data Extraction/filter_csv.py
+++ Data Extraction/filter_csv.py
@@ -5,24 +5,28 @@ csvreader = csv.reader(file)
 
 acceptable_first_column = ["commit", "pull request", "hacker news", "code file"]
 
-python_rows = []
+ids = []
+sourceTypes = []
+urls = []
+languages = []
 
 print("Reading...")
 for row in csvreader:
     if row[0] in acceptable_first_column and row[4] == "Python":
-        python_rows.append(row[30] + "," + row[0] + "," + row[1] + "," + row[4])
-
-python_rows = set(python_rows)
+        ids.append(row[30])
+        sourceTypes.append(row[0])
+        urls.append(row[1])
+        languages.append(row[4])
 
 with open("filtered_big_file.csv", "w", newline="", encoding="utf-8") as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(["ID", "Type", "URL", "RepoLanguage"])
         c_index = 0
-        row_num = len(python_rows)
+        row_num = len(sourceTypes)
         print("Writing... (0 / ", row_num, ")")
-        for row in python_rows:
+        for id, (source, url, language) in enumerate(zip(sourceTypes, urls, languages), start=1):
             print("(", c_index, " / ", row_num, ")")
-            csv_writer.writerow(python_rows)
+            csv_writer.writerow([id, source, url, language])
             c_index += 1
 
 print("Finished")
